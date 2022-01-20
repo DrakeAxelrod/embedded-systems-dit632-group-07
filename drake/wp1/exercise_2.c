d769cb77693b512d9cec1696476831c4a38927df
Drake Axelrod <drake@draxel.io>
2022-01-20 18:23:28 +0000

    Added more to exercise 2
    Almost done

--- drake/wp1/exercise_2.c
+++ drake/wp1/exercise_2.c
@@ -6,9 +6,11 @@
 #include <stdlib.h>
 #include <string.h>
 // function declarations
+// function to rotate one character
 char rotate(char ch, int rotations);
-
+// function to rotate a word (uses rotate)
 char *rotate_word(char *word, int rotations);
+
 /*
  * Main function in the program,
  * @params none
@@ -17,44 +19,65 @@ char *rotate_word(char *word, int rotations);
 int main(int argc, char *argv[]) {
   // char *word = rotate_word(argv[1], atoi(argv[2]));
   // printf("%s", word);
-  char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-  for (int i = 0; i < strlen(alphabet); i++) {
-    printf("%s", rotate(alphabet[i], 1));
+  // char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+  //char *str = "AbCdEFGhIJKLmNOpqRStUVWxYZ";
+  int shifts = atoi(argv[1]);
+  printf("Type something to have it rotated to exit press ctrl+d (linux) or ctrl+z (Windows)\n");
+  char stdin[256];
+  printf(">> ");
+  while (scanf("%15s", stdin) == 1) {
+    //char *word = rotate_word(stdin, atoi(argv[1]));
+    char *word = rotate_word(stdin, shifts);
+    // for(int i = 0; i < argc - 1; i++) {
+    // }
+    printf(">> %s\n", word);
   }
   return 0;
-  // char option[265];
-  // int n = rand_int(1, 100);
-  // prompt the user to choose a number between 1 and 5
-  // printf("Please type in a number between 1-5: ");
-  // read input from console (safer then scanf)
-  // fgets(option, sizeof(option), stdin);
-  // int guess = atoi(option);
-  // string/char ptr to int (-1 is to account for 0 indexing)
-  // int choice = atoi(option) - 1;
-  // printf("%d", n);
-  // successful execution return code
-  // return 0;
 }
 
 // use type casting to rotate character
 // based on a provided number of rotations
-char rotate(char ch, int rotations) {
+// a = 97, z = 122 | A = 65 -> Z = 90 #ref https://www.ascii-code.com/
+char rotate(char ch, int shift) {
   // cast char to a int and add the number of rotations
   // then cast back to char
   int code = (int)ch;
-  int boundary_code = (int)"z";
-  if (code >= boundary_code) {
+  int boundary = (int)'z';
+  for (int i = 0; i < shift; i++) {
+    // if the char code is 90 (Z) we need to circle back to 65 (A)
+    if (code == 90) {
+      // set char to 65 (a)
+      code = 65;
+      // if the char code is 122 (z) we need to circle back to 97 (a)
+    } else if (code == 122) {
+      // set char to 97 (A)
+      code = 97;
+    } else {
+      // if we havent hit and end char code for either capital or lowercase we
+      // increment to the next char code
+      code = code + 1;
+    }
   }
-  char c = (char)(code + rotations);
-  return c;
+  // return the updated char code as a character by casting it back to a char
+  return (char)code;
 }
 
-char *rotate_word(char *word, int rotations) {
-  // loop through the string
+// char *rotate_word(char *word, int shift) {
+char *rotate_word(char *word, int shift) {
+  // get the length of the word to rotate
   int len = strlen(word);
-  char *new_word;
-  for (int i = 0; i < len; i++) {
-    word[i] = rotate(word[i], rotations);
+  // create a pointer for the new rotated word
+  char new_word[len];
+  // we set i here so we can use it to place the end character
+  int i = 0;
+  // loop through the string character by character
+  while ( i <= len) {
+    // replace the char at ith index with the rotated version
+    new_word[i] = rotate(word[i], shift);
+    i++;
   }
-  return word;
+  // add a termination character
+  new_word[i] = '\0';
+  // create a copy of and return a pointer to the copy (since we know the length we can use strndup instead of strdup)
+  return strndup(new_word, len);
 }
