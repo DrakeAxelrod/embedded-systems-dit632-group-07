497fcf23771af9d8449ff153c3e950cf6070d109
Drake Axelrod <drake@draxel.io>
2022-01-20 20:48:21 +0000

    Finished wp1 ex2 I think

--- drake/wp1/exercise_2.c
+++ drake/wp1/exercise_2.c
@@ -5,56 +5,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 // function declarations
 // function to rotate one character
-char rotate(char ch, int rotations);
+char rotchar(char ch, int shifts);
 // function to rotate a word (uses rotate)
-char *rotate_word(char *word, int rotations);
+char *rotword(char *word, int shifts);
 
 /*
  * Main function in the program,
  * @params none
  * @return int (exit code)
  */
-int main(int argc, char *argv[]) {
-  // char *word = rotate_word(argv[1], atoi(argv[2]));
-  // printf("%s", word);
-  // char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-  //char *str = "AbCdEFGhIJKLmNOpqRStUVWxYZ";
-  int shifts = atoi(argv[1]);
-  printf("Type something to have it rotated to exit press ctrl+d (linux) or ctrl+z (Windows)\n");
-  char stdin[256];
-  printf(">> ");
-  while (scanf("%15s", stdin) == 1) {
-    //char *word = rotate_word(stdin, atoi(argv[1]));
-    char *word = rotate_word(stdin, shifts);
-    // for(int i = 0; i < argc - 1; i++) {
-    // }
-    printf(">> %s\n", word);
+int main(int argc, char *argv[])
+{
+  if (argc > 1 && argc < 3)
+  {
+    // convert the arg to an int so we know how many characters to shift by
+    char first_ch = *argv[1];
+    int isNum = isdigit(first_ch) == 2048;
+    if (isNum)
+    {
+      int shifts = atoi(argv[1]);
+      // prompt user and provice context for the program
+      printf("Type something to have it rotated to exit press ctrl+d (linux) or ctrl+z (Windows)\n");
+      // create an array to hold the input (256 is arbitrarly chosen)
+      char stdin[256];
+      //
+      while (scanf("%15s", stdin) == 1)
+      {
+        char *word = rotword(stdin, shifts);
+        printf("%s\n", word);
+        free(word);
+      }
+      return 0;
+    }
+    else
+    {
+      printf("argument is not a valid number (only number)\n");
+      return 1;
+    }
   }
-  return 0;
+  else
+  {
+    printf("no argument provided\n");
+    return 1;
+  }
 }
 
 // use type casting to rotate character
 // based on a provided number of rotations
 // a = 97, z = 122 | A = 65 -> Z = 90 #ref https://www.ascii-code.com/
-char rotate(char ch, int shift) {
+char rotchar(char ch, int shift)
+{
   // cast char to a int and add the number of rotations
   // then cast back to char
   int code = (int)ch;
   int boundary = (int)'z';
-  for (int i = 0; i < shift; i++) {
+  for (int i = 0; i < shift; i++)
+  {
     // if the char code is 90 (Z) we need to circle back to 65 (A)
-    if (code == 90) {
+    if (code == 90)
+    {
       // set char to 65 (a)
       code = 65;
       // if the char code is 122 (z) we need to circle back to 97 (a)
-    } else if (code == 122) {
+    }
+    else if (code == 122)
+    {
       // set char to 97 (A)
       code = 97;
-    } else {
-      // if we havent hit and end char code for either capital or lowercase we
-      // increment to the next char code
+    }
+    // don't rotate numbers
+    else if (code <= 57 && code >= 48)
+    {
+      code = code;
+    }
+    else
+    {
+      // if we havent hit and end char code for either capital or lowercase
+      // we increment to the next char code
       code = code + 1;
     }
   }
@@ -63,7 +93,8 @@ char rotate(char ch, int shift) {
 }
 
 // char *rotate_word(char *word, int shift) {
-char *rotate_word(char *word, int shift) {
+char *rotword(char *word, int shift)
+{
   // get the length of the word to rotate
   int len = strlen(word);
   // create a pointer for the new rotated word
@@ -71,9 +102,10 @@ char *rotate_word(char *word, int shift) {
   // we set i here so we can use it to place the end character
   int i = 0;
   // loop through the string character by character
-  while ( i <= len) {
+  while (i <= len)
+  {
     // replace the char at ith index with the rotated version
-    new_word[i] = rotate(word[i], shift);
+    new_word[i] = rotchar(word[i], shift);
     i++;
   }
   // add a termination character
