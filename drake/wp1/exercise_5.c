5d0e1181d9272f6d59bf01063ca33dd5cd940e26
Drake Axelrod <drake@draxel.io>
2022-01-25 21:00:10 +0000

    Started a tiny bit on ex5

--- drake/wp1/exercise_5.c
+++ drake/wp1/exercise_5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define MAX 100      // Defines the maximum number of the values in the table
 #define MAXNUMBER 20 // Defines the maximum value of random numbers
 // ------ Function declarations ----------
@@ -13,13 +14,20 @@ void count_frequency(int *tab, int *freq);
 // and draws a histogram of the values in that frequency table
 void draw_histogram(int *freq);
 // ------ Function definitions ----------
+int rand_int(int max);
 // ------ Main --------------------------
 // The main entry point for the program
 //
 // If you choose to go for the optional part
 // Please modify it accordingly
-int main(void)
-{
+int main(void) {
+  // random seed init
+  srand(time(0));
   int table[MAX], n;
   int frequency[MAXNUMBER];
+}
+
+int rand_int(int max) {
+  // return a random integer between the min and max value
+  return (rand() % (max - 0 + 1)) + 0;
 }
