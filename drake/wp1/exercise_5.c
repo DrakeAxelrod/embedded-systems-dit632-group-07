a7f766cc767d78b6d413262404da56cb4c4a5262
Drake Axelrod <drake@draxel.io>
2022-01-26 13:22:47 +0000

    fix

--- drake/wp1/exercise_5.c
+++ drake/wp1/exercise_5.c
@@ -42,31 +42,36 @@ int rand_int() {
 
 /* method to fill the table with random numbers */
 void create_random(int *tab) {
-  for (int i = 0; i < MAX; i++) { // loop according to the MAX amount specified in the program
-    tab[i] = rand_int(); // set the number to a random value using the rand_int() function
+  for (int i = 0; i < MAX;
+       i++) { // loop according to the MAX amount specified in the program
+    tab[i] = rand_int(); // set the number to a random value using the
+                         // rand_int() function
   }
 }
 
-/* method to count the frequency of occurrences 
+/* method to count the frequency of occurrences
 of each number in the table of random numbers */
 void count_frequency(int *tab, int *freq) {
-  for (int i = 0; i < MAXNUMBER; i++) { // loop over each number in the frequency table to initialise the frequency to zero
-    freq[i] = 0;
+  for (int i = 0; i < MAXNUMBER;
+       i++) {    // loop over each number in the frequency table
+    freq[i] = 0; // initialise the frequency to zero
   }
-  for (int i = 0; i < MAX; i++) { // loop over each number in the table of random numbers to increment the frequency
-    int val = tab[i]; // find the value of the number
-    freq[val]++; // increment frequency of the number
+  for (int i = 0; i < MAX; i++) { // loop over each number in the table of
+                                  // random numbers to increment the frequency
+    int val = tab[i];             // find the value of the number
+    freq[val]++;                  // increment frequency of the number
   }
 }
 
-/* method to create the histogram that will be displayed as 
+/* method to create the histogram that will be displayed as
 output in the console showing each number and its occurrence. */
 void draw_histogram(int *freq) {
-  for (int i = 0; i < MAXNUMBER; i++) { 
-    if (freq[i] != 0) { // check if the number occurs in the table of random numbers, if not, continue to next number
+  for (int i = 0; i < MAXNUMBER; i++) {
+    if (freq[i] != 0) { // check if the number occurs in the table of random
+                        // numbers, if not, continue to next number
       printf("%d ", i); // print the number
       for (int j = 0; j < freq[i]; j++) { // for each occurrence of the number
-        printf("x"); // print the number's occurrence
+        printf("x");                      // print the number's occurrence
       }
       printf("\n"); // next line
     }
