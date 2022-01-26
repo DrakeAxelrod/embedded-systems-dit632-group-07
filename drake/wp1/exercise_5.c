72b1470f097b5e743e1f6a9c53eb3a8d2f269fb6
Drake Axelrod <drake@draxel.io>
2022-01-26 12:26:27 +0000

    Finishing up wp1

--- drake/wp1/exercise_5.c
+++ drake/wp1/exercise_5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define MAX 100      // Defines the maximum number of the values in the table
 #define MAXNUMBER 20 // Defines the maximum value of random numbers
 // ------ Function declarations ----------
@@ -14,7 +15,7 @@ void count_frequency(int *tab, int *freq);
 // and draws a histogram of the values in that frequency table
 void draw_histogram(int *freq);
 // ------ Function definitions ----------
-int rand_int(int max);
+int rand_int();
 // ------ Main --------------------------
 // The main entry point for the program
 //
@@ -25,9 +26,34 @@ int main(void) {
   srand(time(0));
   int table[MAX], n;
   int frequency[MAXNUMBER];
+  create_random(table);
+  count_frequency(table, frequency);
+  draw_histogram(frequency);
 }
 
-int rand_int(int max) {
+int rand_int() {
   // return a random integer between the min and max value
-  return (rand() % (max - 0 + 1)) + 0;
+  return (rand() % (MAXNUMBER + 1));
+}
+
+void create_random(int *tab) {
+  for (int i = 0; i < MAX; i++) {
+    tab[i] = rand_int();
+  }
+}
+
+void count_frequency(int *tab, int *freq) {
+  for (int i = 0; i < MAX; i++) {
+    int val = tab[i];
+    freq[val] += 1;
+  }
+}
+
+void draw_histogram(int *freq) {
+  for (int i = 0; i < MAXNUMBER; i++) {
+    printf("%d", i);
+    if (freq[i] != 0) {
+      printf("%d %d\n", i, freq[i]);
+    }
+  }
 }
