2c81f058e16cde2a05eecc87c5c6e90e7a6c595b
vernita <gusgouve@student.gu.se>
2022-01-26 17:01:18 +0100

    all the comments

--- turn_ins/bin2hec.c
+++ turn_ins/bin2hec.c
@@ -1,81 +1,49 @@
 // (C) Drake Axelrod, Sicily Ann Brannen, Vernita Gouws, group: 07 (2022)
 // Work package 1
-// Exercise 5
+// Exercise 4_2
 // Submission code : XXXXXX
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef unsigned char byte;
-
-void help();
 void convert(char *in);
-int valid_int(char *str);
+int valid_bin(char *str);
 /*
  * Main function in the program,
- * @params none
+ * @params argc and argv are passed
  * @return int (exit code)
  */
 int main(int argc, char *argv[]) {
-  char *buffer, *input;
-  input = fgets(buffer, 256, stdin);
-  int n = sizeof(input) / sizeof(input[0]);
-  printf("size of input: %d\n", n);
-  // printf("a%s\n", stdin);
-  convert(input);
-  //   int result = valid_int(argv[1]);
-  //   // check if number is float also
-  //   if (argc > 1 && argc < 3) {
-  //     if (result) {
-  //       convert(argv[1]);
-  //     } else {
-  //       printf("failed at result if check\n");
-  //     }
-  //     // if (input == *argv[1]) {
-  //     // } else {
-  //     //   printf("failed on equality");
-  //     // }
-  //   } else {
-  //     printf("must provide one arg\n");
-  //   }
-  // successful execution return code
-  return 0;
-}
-
-void help() { printf("helpful info\n"); }
-
-void convert(char *in) {
-  // char *CHARS = "0123456789ABCDEF";
-  // int value = strtol(in, NULL, 2);
-  //  char *one = { in[0], in[1], in[2], in[3] };
-  //  char *one = { in[5], in[6], in[7], in[8] };
-  int value = (int)strtol(in, NULL, 2);
-
-  // convert integer to hex string
-  char hexString[12]; // long enough for any 32-bit value, 4-byte aligned
-  sprintf(hexString, "0x%x\n", value);
-
-  // output hex string
-  printf(hexString);
-  // while (value != 0)
-  // {
-  //   value >>= 4;
-  //   printf("%c", CHARS[ value & 0xF ]);
-  // }
-  // printf("%d", value);
-  // for (int i = 0; i < 8; i+1) {
-  //   tmp += ();
-  // }
-}
-
-int valid_int(char *str) {
-  while (*str != '\0') {
-    // isdigit return 0 or 1 depending on if char is an int which you check with
-    // 0 if 0 true else 1 false
-    if (!isdigit(*str)) {
-      return 0;
-    }
-    str++;
+  char buffer[256], *input; // create buffer array to support input for piping. create input pointer for use in convert() function
+  if (argc == 1){ // if there is no argument
+  input = fgets(buffer, 256, stdin); // get input from stdin (command line)
+  } else { // if there is an argument
+    input = argv[1]; // get input from argument
   }
-  return 1;
+  if (valid_bin(input)){ // if to check if input is valid binary
+  convert(input); // if valid call convert function
+  } else { // if not valid binary input
+    printf("Invalid binary number.\n"); // error message
+    return 1; // exit program
+  }// call function to convert binary to hexadecimal
+  return 0; // successful execution return code
+}
+
+// method to convert binary number to hex string
+void convert(char *in) { 
+  int value = (int)strtol(in, NULL, 2); // convert input from a string to an integer to a base 2 to represent binary
+  char hex[12]; // create character array for hex representation
+  sprintf(hex, "%x\n", value); // writes values to the hex array
+  printf(hex); // output hex string to console
+}
+
+/* function to check if input is a valid binary representation */
+int valid_bin(char *str) {
+  while (*str != '\0') { // while string has not ended
+    if (*str != '0' && *str != '1') {  // isdigit returns 0 if input is not a number, or 1 if it is a number
+      return 0; // returns 0 if input is not a valid number
+    }
+    str++; // increments to next character in string
+  }
+  return 1; // returns when input is a valid number
 }
--- turn_ins/exercise3.c
+++ turn_ins/exercise3.c
@@ -9,42 +9,48 @@
 int rand(void);
 void guessing();
 
+/*
+ * Main function in the program,
+ * @params argc and argv passed
+ * @return int (exit code)
+ */
 int main(int argc, char *argv[]) {
-  srand(time(0));
-  char answer = 'y';
+  srand(time(0)); // create a random seed
+  char answer = 'y'; // initialize the answer string
 
-  do {
-    guessing();
-    printf("Would you like to play again? y/n\n");
-    scanf("%s", &answer);
-  } while (answer == 'y');
+  do { // continue loop until while condition is false
+    guessing(); // run the guessing function
+    printf("Would you like to play again? y/n\n"); // print to console
+    scanf("%s", &answer); // get the answer
+  } while (answer == 'y'); // continue loop while answer is 'y'
 
   return 0;
 }
 
+/* function to initiate the guessing game */
 void guessing() {
-  int count = 0;
-  int num = rand() % 100 + 1;
-  char input;
-  int guess;
+  int count = 0; // initialize the count of the amount of guesses
+  int num = rand() % 100 + 1; // get a random number from the random number generator
+  char input; // create variable to store user input
+  int guess; // create variable to store the guess
 
   do {
-    printf("%s", "Guess the number.\n");
-    if (scanf("%d", &guess) == 1 && guess >= 1 && guess <= 100) {
-      count++;
-      if (guess == num) {
-        printf("%s %d %s", "Well done! You guessed", count,
-               "times to get the correct answer.\n");
-        break;
-      } else if (guess < num) {
-        printf("Your guess is too low.\n");
-      } else
-        printf("Your guess is too high.\n");
-    } else {
+    printf("%s", "Guess the number.\n"); // print to console
+    if (scanf("%d", &guess) == 1 && guess >= 1 && guess <= 100) { // check whether input is valid
+      count++; // increment count of guesses
+      if (guess == num) { // check whether user guess is equal to the random number
+        printf("%s %d %s", "Well done! You guessed", count, 
+               "times to get the correct answer.\n"); // print success to console
+        break; // end 
+      } else if (guess < num) { // if guess is lower than the random number
+        printf("Your guess is too low.\n"); // print to console
+      } else // if guess is higher than the random number
+        printf("Your guess is too high.\n"); // print to console
+    } else { // if input is not valid
       scanf("%*[^\n]"); // discard that line up to the newline
       printf(
-          "Wrong format. Only numbers between 1-100 (inclusive) accepted.\n");
+          "Wrong format. Only numbers between 1-100 (inclusive) accepted.\n"); // print helpful advice to console
       // continue;
     }
-  } while (guess != num);
+  } while (guess != num); // loop until guess is equal to random number
 }
