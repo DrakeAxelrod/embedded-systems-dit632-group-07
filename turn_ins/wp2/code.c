46ac0cbcea35558bd221363bac667f90e77ac836
Drake Axelrod <drake@draxel.io>
2022-02-02 15:54:30 +0000

    Vernita is a meanie face

--- turn_ins/wp2/code.c
+++ turn_ins/wp2/code.c
@@ -20,22 +20,22 @@ int main(int argc, char const *argv[])
     printf("Not enough arguments passed. You should pass 5 arguments with a space between each one. For example: '1 1 1 1 1'.\n"); // error message
     return 1;                                                                                                                      // return                                                                                                               // exit the program
   }
-  for (int i = 1; i < 6; i++)
+  for (int i = 1; i < 6; i++) // for each argument input
   {
-    if (!check_input(argv[i], i))
+    if (!check_input(argv[i], i)) // calls function to check the user input
     {
       printf("There was an error with your arguments. The minimum value for each argument is 0, and the maximum value is: 1, 7, 3, 1, 1, respectively."); // error message
       exit(0);                                                                                                                                            // exit code
     }
   }
-
+  
   byte value = 0;              // set a byte variable and initialise it to 0
   value += atoi(argv[1]) << 7; // bitshift the first argument and create an integer to represent the 7th binary char
   value += atoi(argv[2]) << 4; // bitshift the second argument and create an integer to represent the 4th-6th binary chars
   value += atoi(argv[3]) << 2; // bitshift the third argument and create an integer to represent the 3rd binary char
   value += atoi(argv[4]) << 1; // bitshift the second argument and create an integer to represent the 2nd binary char
   value += atoi(argv[5]);      // create an integer to represent the 1st binary char
-  printf("%x\n", value);       // print the value which will be the hex representation of the input
+  printf("Hex: %x\n", value);  // print the value which will be the hex representation of the input
   return 0;                    // exit program
 }
 
