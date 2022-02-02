12a250882b21ab1a90c4ff8c0a0b1be64526243c
Drake Axelrod <drake@draxel.io>
2022-02-02 15:50:14 +0000

    Done?!?!?!

--- turn_ins/wp2/code.c
+++ turn_ins/wp2/code.c
@@ -3,15 +3,14 @@
 // Exercise 4_1
 // Submission code : XXXXXX(provided by your TA - s)
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h> // for using atoi
+#include <stdio.h>  // to print
 
 typedef unsigned char byte;
 
 /* function declaration */
 void print_byte_as_bits(char val);
-int check_input(char *input, int value)
+int check_input(char *, int);
 
 /* main function */
 int main(int argc, char const *argv[])
@@ -19,14 +18,14 @@ int main(int argc, char const *argv[])
   if (argc != 6) // if there are too few arguments
   {
     printf("Not enough arguments passed. You should pass 5 arguments with a space between each one. For example: '1 1 1 1 1'.\n"); // error message
-    return 1;                                                                                                                      // exit the program
+    return 1;                                                                                                                      // return                                                                                                               // exit the program
   }
   for (int i = 1; i < 6; i++)
   {
     if (!check_input(argv[i], i))
     {
-      printf("There was an error with your arguments. The minimum value for each argument is 0, and the maximum value is: 1, 7, 3, 1, 1, respectively."); // error mes
-      exit(0);
+      printf("There was an error with your arguments. The minimum value for each argument is 0, and the maximum value is: 1, 7, 3, 1, 1, respectively."); // error message
+      exit(0);                                                                                                                                            // exit code
     }
   }
 
@@ -41,25 +40,31 @@ int main(int argc, char const *argv[])
 }
 
 /*function to check if input is valid*/
-int check_input(char *input, int value)
+int check_input(char *input, int current)
 {
-  if (current == 1 || current == 4 || current == 5)
+  int value = atoi(input);           // first get the int value of the input
+  if (value == 0 && input[0] != '0') // if the input is the char 0 it is valid otherwise if atoi returns a 0 it wasnt a valid input
   {
-    if (input == '1' || input == '0')
+    return 0; // return not valid
+  }
+
+  if (current == 1 || current == 4 || current == 5) // if it's the 1st, 4th, or 5th argument
+  {
+    if (value == 1 || value == 0) // value must be 0 or 1
     {
-      return 1;
+      return 1; // return success
     }
   }
-  else if (current == 2)
+  else if (current == 2) // if it's the 2nd argument
   {
-    if (input >= '0' && input <= '7')
+    if (value >= 0 && value <= 7) // if the value is between 0 and 7
     {
-      return 1;
+      return 1; // return success
     }
   }
-  else if (input >= '0' && input <= '3')
+  else if (value >= 0 && value <= 3) // if it's the 3rd argument and the value is between 0 and 3
   {
-    return 1;
+    return 1; // return success
   }
-  return 0;
+  return 0; // exit code
 }
