34bc2fee0c780592052af776a3619d647db67af2
Drake Axelrod <drake@draxel.io>
2022-02-03 14:19:21 +0000

    Update

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello World!");
+    printf("Hello World 1!");
+    printf("Hello World 2!");
+    printf("Hello World 3!");
+    printf("Hello World 4!");
     return 0;
 }
